a35113fde1a8a6b07ce3a5f1e2e7b856dda5aed2
Angel Arce Carrillo <AngelArce@macbook-pro-de-angel.local>
2016-05-27 19:53:44 -0500

    Initial Commit

---
+++ OneSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneSwift
+//
+//  Created by Angel Arce Carrillo on 27/05/16.
+//  Copyright © 2016 Angel Arce Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
